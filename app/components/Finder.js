400fea448651b59740e5155b1e60b655fbe22657
arsich <mail@arsich.ru>
2016-01-17 19:24:14 +0300

    Refactor components to pure functions

--- app/components/Finder.js
+++ app/components/Finder.js
@@ -2,30 +2,31 @@ import React, { Component, PropTypes }  from 'react';
 
 import {Paper, CardActions, RaisedButton} from 'material-ui';
 
-export default class Finder extends React.Component {
-    render() {
-        const {current, handleSave, handleNext} = this.props;
-        return (
-            <Paper className='content' zDepth={2}>
-                <img className='catImage'
-                     src={current} />
-                <CardActions>
-                    <RaisedButton className='catFinderButton'
-                                  label='Save'
-                                  primary={true}
-                                  onClick={handleSave}/>
-                    <RaisedButton className='catFinderButton'
-                                  label='Next'
-                                  secondary={true}
-                                  onClick={handleNext} />
-                </CardActions>
-            </Paper>
-        );
-    }
-}
+const Finder = ({
+    current,
+    handleSave,
+    handleNext
+}) => (
+    <Paper className='content' zDepth={2}>
+        <img className='catImage'
+             src={current} />
+        <CardActions>
+            <RaisedButton className='catFinderButton'
+                          label='Save'
+                          primary={true}
+                          onClick={handleSave}/>
+            <RaisedButton className='catFinderButton'
+                          label='Next'
+                          secondary={true}
+                          onClick={handleNext} />
+        </CardActions>
+    </Paper>
+);
 
 Finder.propTypes = {
     current: PropTypes.string,
     handleSave: PropTypes.func.isRequired,
     handleNext: PropTypes.func.isRequired
 };
+
+export default Finder;
--- app/components/Viewer.js
+++ app/components/Viewer.js
@@ -4,32 +4,34 @@ import {Paper, GridList, GridTile, Dialog} from 'material-ui';
 
 import Immutable from 'immutable';
 
-export default class Viewer extends React.Component {
-    render() {
-        const {history, handleShow} = this.props;
-        const createGridTile = function (url, key) {
-            return (
-                <GridTile key={'cat_item_' + key}
-                          onClick={handleShow.bind(null, url)}
-                          className='gridTile'>
-                    <img src={url} className='gridTileImg'/>
-                </GridTile>
-            );
-        };
+const Viewer = ({
+    history,
+    handleShow
+}) => {
+    const createGridTile = function (url, key) {
         return (
-            <Paper className='content' zDepth={2}>
-                {history && history.size ?
-                    <GridList cols={2} cellHeight={300} className='catsGrid'>
-                        {history.map(createGridTile)}
-                    </GridList>
-                    : <p>Let&#39;s save some cats!</p>
-                }
-            </Paper>
+            <GridTile key={'cat_item_' + key}
+                      onClick={handleShow.bind(null, url)}
+                      className='gridTile'>
+                <img src={url} className='gridTileImg'/>
+            </GridTile>
         );
-    }
-}
+    };
+    return (
+        <Paper className='content' zDepth={2}>
+            {history && history.size ?
+                <GridList cols={2} cellHeight={300} className='catsGrid'>
+                    {history.map(createGridTile)}
+                </GridList>
+                : <p>Let&#39;s save some cats!</p>
+            }
+        </Paper>
+    );
+};
 
 Viewer.propTypes = {
     history: React.PropTypes.instanceOf(Immutable.List),
     handleShow: PropTypes.func.isRequired
 };
+
+export default Viewer;
